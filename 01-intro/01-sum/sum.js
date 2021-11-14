825575ee2cd55ac67887985f436afbb87eb9024d
akhvatov <superbus414@gmail.com>
2021-11-14 09:40:11 +0300

    nodejs-20210907_superbus414 task 1 and 2

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,11 @@
 function sum(a, b) {
   /* ваш код */
+  let sum = a + b;
+ if (typeof sum == 'number'){
+   return sum;
+ } else {
+   throw new TypeError ('Not a number')
+ }
 }
 
 module.exports = sum;
