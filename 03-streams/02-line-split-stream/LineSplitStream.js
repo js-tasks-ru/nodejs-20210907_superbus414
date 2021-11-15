194d583ee3566568651b1a00d416cc538a432711
akhvatov <superbus414@gmail.com>
2021-11-15 20:25:05 +0300

    solutions module 2 and 3

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,32 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.str = '';
+    this.newStr = [];
+    this.lineBreak = os.EOL;
+    this.endcoding = options.end
   }
 
   _transform(chunk, encoding, callback) {
+    this.str += chunk.toString();
+    this.newStr = this.str.split(this.lineBreak);
+    this.str = this.newStr.pop();
+    this.newStr.forEach((li) => {
+      if(li){
+        this.push(li);
+      }
+    });
+    callback(null);
+    console.log(chunk);
+    console.log(this.str);
   }
 
   _flush(callback) {
+    if(this.str){
+      this.push(this.str);
+    }
+    console.log(this.str);
+    callback(null);
   }
 }
 
